c71bcc1a5a99e847e89e842abc285b517a62b42a
artisharma9260 <poojapooja2780@gmail.com>
2025-12-06 07:33:24 +0000

    best time to buy and sell stock

---
+++ Arrays/best_time_to_buy_and_sell_stock.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int maxprofit = 0; 
+
+        for(int i = 0; i < prices.size(); i++) {
+            for(int j = i + 1; j < prices.size(); j++) {
+                int profit = prices[j] - prices[i]; 
+                maxprofit = max(maxprofit, profit);
+            }
+        }
+
+        return maxprofit;
+    }
+};
