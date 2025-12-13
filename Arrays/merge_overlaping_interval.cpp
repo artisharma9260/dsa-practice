ded56f072e9280cef6700ec2fde71047b61f318f
artisharma9260 <poojapooja2780@gmail.com>
2025-12-13 15:34:20 +0000

    merge interval

---
+++ Arrays/merge_overlaping_interval.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end());
+        vector<vector<int>> ans;
+        int n = intervals.size();
+        int i = 0;  
+        while (i < n) {
+            int start = intervals[i][0];
+            int end = intervals[i][1];
+            int j = i + 1;
+            while (j < n && intervals[j][0] <= end) {
+                end = max(end, intervals[j][1]);
+                j++;
+            }
+            ans.push_back({start, end});
+            i = j;  
+        }
+        return ans;
+    }
+};
