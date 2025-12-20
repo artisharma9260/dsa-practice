4a355b32dbe7164f3fb74d42944286ca2eb11c53
artisharma9260 <poojapooja2780@gmail.com>
2025-12-20 09:09:51 +0000

    floar and ciel using binary search

---
+++ Binary_search/Floor_ciel.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+class FloarCeilFinder{
+public:
+    int findFloor(int arr[],int n,int x){
+        int low = 0,high = n-1;
+        int ans = -1;
+        while(low <= high){
+            int mid = (low+high) / 2;
+            if(arr[mid] <= x){
+                ans = arr[mid];
+                low = mid+1;
+            }
+            else{
+                high = mid-1;
+            }
+        }
+        return ans;
+    }
+    int findCeil(int arr[],int n,int x){
+        int low = 0,high = n-1;
+        int ans = -1;
+        while(low <= high){
+            int mid = (low+high) / 2;
+            if(arr[mid] >= x){
+                ans=arr[mid];
+                high=mid-1;
+            }else {
+                low = mid+1;
+            }
+        }
+        return ans;
+    }
+};
