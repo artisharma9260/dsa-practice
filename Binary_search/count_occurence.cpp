20cae5ea5a8527765ed0d773724ff895b755169b
artisharma9260 <poojapooja2780@gmail.com>
2025-12-23 16:52:20 +0000

    count occurence

---
+++ Binary_search/count_occurence.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int count(vector<int>& arr,int n,int x){
+    int cnt = 0;
+    for(int i = 0; i < n; i++){
+        if (arr[i] == x) cnt++;
+    }
+    return cnt;
+}
