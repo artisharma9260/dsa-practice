497a27532dbb4ae57736ca531b73d6b0c7f0b8f8
artisharma9260 <poojapooja2780@gmail.com>
2026-01-10 10:50:09 +0000

    jump game by greedy algorithm

---
+++ grredy_algorithm/jump_game.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int maxIndex = 0;
+
+        for(int i = 0; i < nums.size(); i++){
+            if(i > maxIndex) {
+                return false;
+            }
+            maxIndex = max(maxIndex, i+nums[i]);
+        }
+        return true;
+        
+    }
+};
