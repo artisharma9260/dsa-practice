6cc9fa8427755b02b6db89323f1c47fb95e22eba
artisharma9260 <poojapooja2780@gmail.com>
2026-01-15 09:08:06 +0000

    delete node in a linked list when head is not given and its not a tail node also

---
+++ LinkedList/deleteNodewithouthead.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        ListNode* temp = node->next;
+        node->val = temp->val;
+        node->next=temp->next;
+        delete temp;
+    }
+};
