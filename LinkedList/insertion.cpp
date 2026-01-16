f39ebe4db55c3a1b1e4004bbca4f89df5034db07
artisharma9260 <poojapooja2780@gmail.com>
2026-01-16 05:39:26 +0000

    insertion in a singly linked list

---
+++ LinkedList/insertion.cpp
@@ -0,0 +1,93 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node {
+    public:
+    int data;
+    Node* next;
+    
+    public:
+    Node(int data1,Node* next1) {
+        data = data1;
+        next = next1;
+    }
+    Node(int data1){
+        data = data1;
+        next=nullptr;
+    }
+};
+Node* convertArr2LL(vector<int>& arr){
+    Node* head = new Node(arr[0]);
+    Node* mover = head;
+    for(int i =1; i<arr.size();i++){
+        Node* temp = new Node(arr[i]);
+        mover->next = temp;
+        mover = temp;
+    }
+    return head;
+}
+Node* insertHead(Node* head,int val){
+    Node* temp = new Node(val,head);
+    return temp;
+}
+Node* insertTail(Node* head,int val){
+    if(head == NULL){
+        return new Node(val);
+    }
+    Node* temp = head;
+    while(temp->next != NULL){
+        temp=temp->next;
+    }
+    temp->next = new Node(val);
+    return head;
+}
+Node* insertPosition(Node* head,int el,int k){
+    if(head ==NULL) {
+        if(k == 1){
+            return new Node(el);
+        }else{
+            return head;
+        }
+    }
+    if(k==1) {
+        return new Node(el,head);
+    }
+    int cnt = 0;
+    Node* temp = head;
+    while(temp != NULL){
+        cnt++;
+        if(cnt == (k-1)) {
+            Node* x = new Node(el,temp->next);
+            temp->next = x;
+            break;
+        }
+        temp = temp->next;
+    }
+    return head;
+}
+Node* insertBeforeValue(Node* head,int el,int val){
+    if(head ==NULL) {
+        return NULL;
+    }
+    if(head->data==val) {
+        return new Node(el,head);
+    }
+    Node* temp = head;
+    while(temp->next != NULL){
+        if(temp->next->data == val) {
+            Node* x = new Node(el,temp->next);
+            temp->next = x;
+            break;
+        }
+        temp = temp->next;
+    }
+    return head;
+}
+
+int main() {
+    vector<int> arr = {2,5,8,7};
+   Node* head = convertArr2LL(arr);
+   head= insertTail(head,100);
+   cout<<head->data;
+
+    return 0;
+}
