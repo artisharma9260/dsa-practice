58efece7ed581bcd4aafec01b6746c215359a748
artisharma9260 <poojapooja2780@gmail.com>
2026-01-23 18:25:44 +0000

    modified

--- Binary_search/find_smallest_divisor.cpp
+++ Binary_search/find_smallest_divisor.cpp
@@ -12,19 +12,19 @@ public:
     }
     int bs(vector<int>&nums,int threshold){
         int low=1;
-        int ans=-1;
+        int ans1=-1;
         int high =*max_element(nums.begin(),nums.end());
         while(low<=high){
             int mid=(low+high)/2;
             if(sumbyd(nums,mid)<=threshold){
-                ans=mid;
+                ans1=mid;
                 high=mid-1;
             }
             else{
                 low=mid+1;
             }
         }
-        return ans;
+        return ans1;
     }
     int smallestDivisor(vector<int>& nums, int threshold) {
         return bs(nums,threshold);
