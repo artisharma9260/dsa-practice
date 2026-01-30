b50ddd90618af54d0374405f4cb8e7a3ae5ec1e0
artisharma9260 <poojapooja2780@gmail.com>
2026-01-30 10:12:14 +0000

    remove outer paranthesis

---
+++ Strings/RemoveOuterParanthesis.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+
+        string result = "";
+        int level = 0;
+        for(char ch : s){
+            if(ch == '('){
+                if(level > 0) result += ch;
+                level++;
+            }
+            else if (ch == ')'){
+                level--;
+                if(level > 0) result += ch;
+            }
+        }
+        return result;
+        
+    }
+};
