c3d91c51b49198188bc4e6dcdaf6f386c537d643
artisharma9260 <poojapooja2780@gmail.com>
2026-02-09 11:43:38 +0000

    frog jump using tabulation

---
+++ dynamicProgramming/FrogJumpTabulation.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int frogJump(const vector<int>& height) {
+        if(height.empty()) return 0;
+        
+        int n = (int)height.size();
+        vector<int>dp(n,INT_MAX);
+        dp[0] = 0;
+        for(int ind = 1; ind < n; ind++){
+            int jumpOne = dp[ind-1] + abs(height[ind]-height[ind-1]);
+            int jumpTwo = INT_MAX;
+            if(ind > 1){
+                jumpTwo = dp[ind-2] + abs(height[ind]-height[ind-2]);
+            }
+            dp[ind] = min(jumpOne,jumpTwo);
+        }
+        return dp[n-1];
+    }
+};
