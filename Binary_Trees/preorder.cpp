0f0e5059724d48f6943ca98ed227036371b26c6d
artisharma9260 <poojapooja2780@gmail.com>
2026-03-22 17:04:31 +0000

    preorder traversal

---
+++ Binary_Trees/preorder.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    void preorder(TreeNode* root,vector<int>& result){
+        if(root == NULL) return;
+        result.push_back(root->val);
+        preorder(root->left,result);
+        preorder(root->right,result);
+    }
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int>result;
+        preorder(root,result);
+        return result;
+    }
+};
