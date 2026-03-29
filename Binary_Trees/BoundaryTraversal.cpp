8d6988ae3195a618e62c6e814ca581e96d27ed36
artisharma9260 <poojapooja2780@gmail.com>
2026-03-29 07:17:07 +0000

    Boundary traversal

---
+++ Binary_Trees/BoundaryTraversal.cpp
@@ -0,0 +1,60 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val){
+        data = val;
+        left = right = NULL;
+    }
+};
+class solution{
+public:
+    bool isLeaf(Node* node){
+    return (node->left == NULL && node->right == NULL);
+    }
+    void addLeftBoundary(Node* root,vector<int>& res){
+        Node* cur = root->left;
+        while(cur){
+            if(!isLeaf(cur)) res.push_back(cur->data);
+            if(cur->left) cur = cur->left;
+            else cur = cur->right;
+        }
+    }
+    void addRightBoundary(Node* root,vector<int> &res){
+        Node* cur = root->right;
+        vector<int> tmp;
+        while(cur) {
+            if(!isLeaf(cur)) tmp.push_back(cur->data);
+            if(cur->right) cur = cur->right;
+            else cur = cur->left;
+        }
+        for(int i = tmp.size()-1;i >= 0; --i){
+            res.push_back(tmp[i]);
+        }
+        
+    }
+    void addLeaves(Node* root, vector<int>& res){
+        if(isLeaf(root)){
+            res.push_back(root->data);
+            return;
+        }
+        if(root->left) addLeaves(root->left,res);
+        if(root->right) addLeaves(root->right,res);
+    }
+    vector<int>printBoundary(Node* root){
+        vector<int> res;
+        if(!root) return res;
+        if(!isLeaf(root)) res.push_back(root->data);
+        addLeftBoundary(root,res);
+        addLeaves(root,res);
+        addRightBoundary(root,res);
+        return res;
+    }
+};
+int main(){
+    return 0;
+}
